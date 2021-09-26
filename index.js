e3d7e9130439a9e495a052731e32732474872f27
Wilfred <wilfredbayudan@gmail.com>
2021-09-26 10:49:14 -1000

    commit tree 01241d040c36173202f516d626dff44186cd69c3

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correspodning letter from the end
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    // if any letters don't match return false
+    if (startChar !== endChar) return false
+  }
+  // return true
+  return true;
+
 }
 
 /* 
